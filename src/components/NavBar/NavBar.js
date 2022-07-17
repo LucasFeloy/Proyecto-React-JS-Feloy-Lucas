f48ac67efc76ecd6129fff8142d37f74b4fba6d2
Lucas Feloy <lucas.d.feloy@gmail.com>
2022-07-16 21:31:22 -0300

    segunda-entregs

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+import './NavBar.css'
+
+function NavBar(){
+    return(
+        <div>
+            <h1>FRIKI-REMERAS</h1>
+            <ul>
+                <li>NOSOTROS</li>
+                <li>REMERAS</li>
+                <li>GUIA DE TALLES</li>
+                <li>CONTACTO</li>
+            </ul>
+        </div>
+
+    )
+}
+
+export default NavBar;
